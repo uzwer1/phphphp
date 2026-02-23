b6799f81a61adb7a71f22bca7861d89a3c1ec6bb
kamrad001 <kamrad001@yandex.ru>
2026-02-23 19:03:33 +0400

    First Modify index.php

--- 01/index.php
+++ 01/index.php
@@ -1,3 +1,11 @@
-<?php 
-echo('Hello, World!')
-?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPHPHP</title>
+</head>
+<body>
+<h1><?=</h1>
+</body>
+</html>
